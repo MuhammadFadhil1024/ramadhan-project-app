f735605b6f83bbc53dfd53fd1dc5d44f282d1ba8
fadhil <fadhilnur102001@gmail.com>
2025-03-11 18:35:18 +0700

    temp

---
+++ resources/views/surah.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, in harum? Consequuntur, sapiente porro
+            perspiciatis consectetur alias enim praesentium labore dolor dicta minima mollitia soluta commodi
+            repellat
+            ullam? Vero, ullam!</h1>
+    </h1>
+@endsection
